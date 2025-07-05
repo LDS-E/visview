e0af24280d9f2f944ba0b11ff4eeb80fe3349fe0
Lucas Eifler <lucass.eifler@gmail.comm>
2025-07-05 20:27:17 +0200

    edited NavBar Layout, thinner and more responsive.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,40 +3,50 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav className="bg-secondary p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="flex mr-4">
-          <Link href="/" className="flex items-center">
-            <Image
-              src="/images/LogoT.png"
-              alt="VisView Logo"
-              width={150}
-              height={50}
-              objectFit="contain"
-            />
-          </Link>
-        </div>
-        <ul className="flex space-x-6 text-white">
+    <nav className="bg-secondary py-2 shadow-md">
+      <div className="container mx-auto flex items-center justify-between h-16">
+        {/* Logo */}
+        <Link href="/">
+          <Image
+            src="/images/LogoT.png"
+            alt="VisView Logo"
+            width={120}
+            height={40}
+            className="object-contain"
+          />
+        </Link>
+
+        {/* Links */}
+        <ul className="flex space-x-6 text-white text-base font-medium items-center">
           <li>
-            <Link href="/" className="hover:text-accent">
+            <Link
+              href="/"
+              className="transition duration-300 hover:text-accent hover:underline underline-offset-4"
+            >
               Home
             </Link>
           </li>
           <li>
-            <Link href="/about" className="hover:text-secondary transition">
+            <Link
+              href="/about"
+              className="transition duration-300 hover:text-accent hover:underline underline-offset-4"
+            >
               About
             </Link>
           </li>
           <li>
             <Link
               href="/newsletter"
-              className="hover:text-secondary transition"
+              className="transition duration-300 hover:text-accent hover:underline underline-offset-4"
             >
               Newsletter
             </Link>
           </li>
           <li>
-            <Link href="/posts" className="hover:text-secondary transition">
+            <Link
+              href="/posts"
+              className="transition duration-300 hover:text-accent hover:underline underline-offset-4"
+            >
               All Posts
             </Link>
           </li>
