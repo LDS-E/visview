29059872de74ba0fe76d56834afea1edbc912b04
Lucas Eifler <lucass.eifler@gmail.comm>
2025-08-07 19:08:04 +0200

    edited to build on netlify.

---
+++ src/types/blogpost.tsx
@@ -0,0 +1,11 @@
+export interface BlogPost {
+  sys: {
+    id: string;
+  };
+  fields: {
+    title: string;
+    slug: string;
+    excerpt?: string;
+    category?: string; // Propriedade opcional, pois nem todos os posts podem ter.
+  };
+}
